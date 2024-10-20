66a76ec608110bdc5996e4cb49ebdbe4b461b30a
kompon <komponnutaro0@gmail.com>
2024-10-20 11:19:44 +0700

    88888

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
+// server.js หรือ app.js
 const express = require('express');
-const sequelize = require('./config/db'); // นำเข้า Sequelize สำหรับเชื่อมต่อฐานข้อมูล
-const routes = require('./routes/productRoutes'); // นำเข้ารูทสำหรับผลิตภัณฑ์
+const cors = require('cors'); // เพิ่ม cors
+const sequelize = require('./config/db');
+const routes = require('./routes/productRoutes');
 const app = express();
 
-app.use(express.json()); // รองรับ JSON ในคำขอ
+app.use(cors()); // เพิ่ม middleware CORS
+app.use(express.json());
 
-// ระบุตำแหน่ง URL สำหรับเรียกดูรูปภาพ
-app.use(express.static('public')); // ให้บริการไฟล์จากโฟลเดอร์ public
-app.use('/images', express.static('public/images')); // ให้บริการไฟล์รูปภาพจากโฟลเดอร์ public/images
+// ระบุตำแหน่ง url สำหรับเรียกดูรูปภาพ
+app.use(express.static('public'));
+app.use('/images', express.static('images'));
 
-// ระบุตำแหน่ง URL สำหรับเรียกใช้งาน API
-app.use('/api', routes); // ใช้เส้นทางที่กำหนดใน productRoutes
+// ระบุตำแหน่ง url สำหรับเรียกใช้งาน api
+app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000; // ตั้งค่าพอร์ต
-
-// ซิงค์กับฐานข้อมูลและเริ่มเซิร์ฟเวอร์
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+const PORT = process.env.PORT || 3000;
+sequelize.sync({force: false}).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 }).catch(error => console.error(error));
